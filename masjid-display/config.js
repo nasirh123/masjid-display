fb19c8a57c24ce13a35de8314747e3f8237c57b9
nasirh123 <80854346+nasirh123@users.noreply.github.com>
2026-03-15 16:30:07 -0700

    Add files via upload

---
+++ masjid-display/config.js
@@ -0,0 +1,15 @@
+const SETTINGS = {
+
+title: "The Future Home of Islamic Center of Federal Way",
+
+goal: 50000,
+
+qrLink: "https://donate.stripe.com/9B66oI80p8k42I19P54Rq01",
+
+languages: "Please Donate during the blessed Ramadan",
+
+mediaInterval: 10000,
+
+milestones: [25,50,75,100]
+
+}
